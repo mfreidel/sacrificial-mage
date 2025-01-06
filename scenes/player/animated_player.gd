d838a75a8b23427244131e2f061ed6fab2b420d7
mfreidel <freidel.michael@gmail.com>
2025-01-06 17:57:01 -0600

    Add initial melee attack code

--- scenes/player/animated_player.gd
+++ scenes/player/animated_player.gd
@@ -1,14 +1,34 @@
 extends CharacterBody2D
 
+# Variables for ranged attack
 @onready var level_node = get_tree().get_root().get_node("Level")
 @onready var projectile = load("res://scenes/entities/projectile.tscn")
 
+# Variables for weapon switching
+var player_weapon = 0
+var weapons_list = ["melee", "ranged"]
+
+# Base damage value for melee attack
+var base_melee_damage = 2.0
+
+# Movement speed is used to calculate velocity while moving.
 var movement_speed = 1.5
+
+# facing_rot simulates object rotation
 var facing_rot = 3.14159
+# Player sprite should never rotate according to its properties.
+# Instead, this value gets updated during movement handling to determine 
+# which direction the sprite animation is facing
+
+
+func _ready():
+	pass
 
 func handle_input():
 	
 	var move_direction = Vector2.ZERO
+	
+	# Movement input
 	if Input.is_action_pressed("right"):
 		move_direction.x += 1.0
 		facing_rot = 1.5708
@@ -21,16 +41,30 @@ func handle_input():
 	elif Input.is_action_pressed("up"):
 		move_direction.y -= 1.0
 		facing_rot = 0.0
+	
+	# Apply movement direction to MeleeWeapon rotation based on facing_rot
+	$MeleeWeapon.rotation = facing_rot
+	
+	# Attack input
 	if Input.is_action_just_pressed("attack"):
-		shoot()
+		if (weapons_list[player_weapon] == "ranged"):
+			shoot()
+		elif (weapons_list[player_weapon] == "melee"):
+			# Call movement animation. Animation will use a signal to call damage code
+			$MeleeWeapon/MeleeAnimationTree.get("parameters/playback").travel("swing")
+	
+	# Calculate velocity
 	velocity = move_direction * movement_speed
 	
+	# Movement animation
 	if velocity == Vector2.ZERO:
 		$AnimationTree.get("parameters/playback").travel("Idle")
 	else:
 		$AnimationTree.get("parameters/playback").travel("Walk")
 		$AnimationTree.set("parameters/Idle/blend_position", move_direction)
 		$AnimationTree.set("parameters/Walk/blend_position", move_direction)
+	# 
+	# END OF handle_input() function
 
 func shoot():
 	var instance = projectile.instantiate()
@@ -38,8 +72,16 @@ func shoot():
 	instance.spawnPos = global_position
 	instance.spawnRot = facing_rot
 	level_node.add_child.call_deferred(instance)
+	
+func melee_damage(target_object):
+	if target_object is CharacterBody2D:
+		# Making some assumptions here. This could probably use some improvement.
+		print("Hit! :: " + str(target_object))
+		target_object.HEALTH -= base_melee_damage
 
-func _physics_process(delta: float) -> void:
+func next_weapon():
+	pass
+
+func _physics_process(_delta: float) -> void:
 	handle_input()
-	# look_at(get_global_mouse_position())
 	move_and_collide(velocity)
