23123acb159f86ba08abf16620a8ab2702f19b7e
Michael J. Freidel <32282481+mfreidel@users.noreply.github.com>
2025-01-15 07:38:44 -0600

    Add stop condition and restart logic to enemy_spawner.gd

--- scenes/entities/enemy_spawner.gd
+++ scenes/entities/enemy_spawner.gd
@@ -2,17 +2,29 @@ extends Node2D
 
 signal enemy_spawned
 
+# Total number of enemies to spawn
+@export var MAX_SPAWNS : int = 5
+
 @onready var res_enemy = preload("res://scenes/entities/enemy.tscn")
 
+var spawn_count : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$SpawnTimer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
+	
+
+func _restart_spawner() -> void:
+	if !($SpawnTimer.is_stopped()):
+		$SpawnTimer.stop()
+	spawn_count = 0
+	$SpawnTimer.start()
+
 
 func spawn_enemy() -> void:
 	var inst_enemy = res_enemy.instantiate()
@@ -22,4 +34,8 @@ func spawn_enemy() -> void:
 	emit_signal("enemy_spawned")
 
 func _on_spawn_timer_timeout() -> void:
-	spawn_enemy()
+	spawn_count += 1
+	if spawn_count > MAX_SPAWNS:
+		$SpawnTimer.stop()
+	else:
+		spawn_enemy()
