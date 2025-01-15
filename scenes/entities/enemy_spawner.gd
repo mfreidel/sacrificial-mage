a5cd1d43f127af716369d8e096f06153aa89223c
Michael J. Freidel <32282481+mfreidel@users.noreply.github.com>
2025-01-15 06:14:16 -0600

    Add simple logic to spawner

---
+++ scenes/entities/enemy_spawner.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var res_enemy = preload("res://scenes/entities/enemy.tscn")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+
+func _on_spawn_timer_timeout() -> void:
+	var inst_enemy = res_enemy.instantiate()
+	inst_enemy.position = position
+	inst_enemy.PATH_TARGET = get_parent().get_node("AnimatedPlayer")
+	get_parent().add_child(inst_enemy)
