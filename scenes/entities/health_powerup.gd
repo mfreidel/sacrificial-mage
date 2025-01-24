6d47d789f24a8cf18106efc5d9aa749dd220a7f2
Michael J. Freidel <32282481+mfreidel@users.noreply.github.com>
2025-01-23 23:14:15 -0600

    Make HealthPowerup disappear when colliding with player

--- scenes/entities/health_powerup.gd
+++ scenes/entities/health_powerup.gd
@@ -1,5 +1,15 @@
 extends CharacterBody2D
 
+@onready var player_node = get_tree().get_first_node_in_group("player")
 
 func _ready() -> void:
 	$AnimationPlayer.play("rotate")
+
+func _physics_process(_delta) -> void:
+	velocity = Vector2(0, 0)
+	var collision = move_and_slide()
+	if collision:
+		var col_target = get_last_slide_collision().get_collider()
+		if col_target is CharacterBody2D:
+			#heal_player()
+			queue_free()
