b28345975f7d5b4cbd15726e2fdba522b44fdc57
Michael J. Freidel <32282481+mfreidel@users.noreply.github.com>
2025-01-25 08:50:34 -0600

    Attach empty scripts to PlayerHUD nodes

---
+++ scenes/player/hud_main_panel.gd
@@ -0,0 +1,10 @@
+extends PanelContainer
+## MainPanel script for PlayerHUD
+
+# built-in
+func _ready() -> void:
+	pass
+
+# built-in
+func _process(delta: float) -> void:
+	pass
---
+++ scenes/player/player_hud.gd
@@ -0,0 +1,10 @@
+extends CanvasLayer
+## Base-level script for PlayerHUD Node.
+
+# built-in
+func _ready() -> void:
+	pass
+
+# built-in
+func _process(delta: float) -> void:
+	pass
