c48c68c00255630fab5419a2e244ff492817f086
Michael J. Freidel <32282481+mfreidel@users.noreply.github.com>
2025-01-27 16:21:03 -0600

    Add credits scene to StartMenu

--- scenes/start_menu/start_menu.gd
+++ scenes/start_menu/start_menu.gd
@@ -5,3 +5,7 @@ func _on_start_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits/credits.tscn")
