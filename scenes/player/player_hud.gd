8458a4f73fa2691a496bbfe01df9b7f1ed720f34
Michael J. Freidel <32282481+mfreidel@users.noreply.github.com>
2025-01-28 06:06:36 -0600

    Remove all Quit buttons from UI scenes
    They essentially crash the game when clicked in HTML5 exports

--- scenes/player/player_hud.gd
+++ scenes/player/player_hud.gd
@@ -96,8 +96,6 @@ func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/start_menu/start_menu.tscn")
 
 
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
 
 
 func _on_player_death() -> void:
--- scenes/start_menu/start_menu.gd
+++ scenes/start_menu/start_menu.gd
@@ -15,9 +15,5 @@ func get_text_file_content(filePath):
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/Mountain_Range.tscn")
 
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
-
-
 func _on_credits_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits/credits.tscn")
